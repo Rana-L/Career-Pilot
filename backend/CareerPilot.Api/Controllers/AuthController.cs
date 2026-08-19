dfcf6377ad019b90f021eba63d088fa2e24a7d01
Rana Rehman Liaqat <101597634+Rana-L@users.noreply.github.com>
2026-08-19 14:18:58 +0100

    Adding the services and dto

---
+++ backend/CareerPilot.Api/Controllers/AuthController.cs
@@ -0,0 +1,8 @@
+using CareerPilot.Api.data;
+using CareerPilot.Api.dtos;
+using CareerPilot.Api.models;
+using CareerPilot.Api.services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareerPilot.Api.Controllers;
---
+++ backend/CareerPilot.Api/dto/RegisterRequest.cs
@@ -0,0 +1,7 @@
+namespace CareerPilot.Api.dto;
+
+public class RegisterRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
